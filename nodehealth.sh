8916654d9eb2e286ad42ffa640fcf2c0d8e783f7
Balamanikanta Anantha <bala@Balas-MacBook-Pro.local>
2025-12-02 15:46:59 -0600

    Removed echo statements and added debug mode

--- nodehealth.sh
+++ nodehealth.sh
@@ -4,19 +4,18 @@
 #Author: Balamanikanta Anantha
 #Date: 12-02-2025
 #This Scripts shows node health of my system
-#Version:v1
+#Version:v2
 ################
 
-echo "print disk space"
+set -x #to start the debug mode
+
+
 df -h
 
-echo "print no.of cpu's"
 sysctl -n hw.ncpu
 
-echo "print no.of logical cpu's"
 sysctl -n hw.logicalcpu
 
-echo "print no.of physical cpu's"
 sysctl -n hw.physicalcpu
 
 
