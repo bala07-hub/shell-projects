06cf40d7c67556d4043cbcd2a63185049ab0d148
Balamanikanta Anantha <bala@Balas-MacBook-Pro.local>
2025-12-03 13:28:08 -0600

    made changes with exo commands -v3

--- nodehealth.sh
+++ nodehealth.sh
@@ -4,10 +4,12 @@
 #Author: Balamanikanta Anantha
 #Date: 12-02-2025
 #This Scripts shows node health of my system
-#Version:v2
+#Version:v3
 ################
 
 set -x #to start the debug mode
+set -e  #to identify and stop script execution if there are any errors in the script
+set -o pipefail # to identify errors in pipecommands
 
 
 df -h
@@ -17,6 +19,8 @@ sysctl -n hw.ncpu
 sysctl -n hw.logicalcpu
 
 sysctl -n hw.physicalcpu
+
+ps -ef | grep amazon | awk -F" " '{print $2}'
 
 
 
